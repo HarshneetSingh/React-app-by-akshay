100741c36345b357557e262a47e96d579062f42b
Harshneet <harshneets50@gmail.com>
2022-12-31 16:27:42 +0530

    class 1

---
+++ app.js
@@ -0,0 +1,21 @@
+const header1 = React.createElement(
+    "h2",// HTML elementTAG
+    null,// PROPS you need to send like class and all
+    "Harshneet" // children you want to display
+);
+const header2 = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Harshneet"
+    ));
+const container = React.createElement(
+    "div",
+    { className: "container" },
+    [header1, header2]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root")); // 
+root.render(container);
