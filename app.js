ca95277ae2675f3ef045d78fddd6ad3e1511bb2f
Harshneet <harshneets50@gmail.com>
2023-02-16 01:15:18 +0530

    CLASS 3 is done

--- app.js
+++ app.js
@@ -1,23 +1,35 @@
 import React from "react";
-import  ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client";
+
+// header1 is the object that turns into html code and become as (DOM element) ...
+
 const header1 = React.createElement(
     "h2",// HTML elementTAG
     null,// PROPS you need to send like class and all
     "Harshneet" // children you want to display
 );
-const header2 = React.createElement(
-    "div",
-    null,
-    React.createElement(
-        "h1",
-        null,
-        "Harshneet"
-    ));
-const container = React.createElement(
-    "div",
-    { className: "container" },
-    [header1, header2]
+
+
+
+// JSX = > React.createElement => object => html(dom).... here babel helps jsx to turn into React.createElement
+const header2 = (
+    <h1>namaste react</h1>
 );
 
+// header3 is an function that returns a jsx 
+const Header3 =()=> (
+    <h1>namaste react</h1>
+);
+const HeaderComponent =()=>{
+    return (
+        <div>
+            {Header3()}
+            {header1}
+            {<Header3/>}
+        </div>
+    )
+};
 const root = ReactDOM.createRoot(document.getElementById("root")); // 
-root.render(container);
+
+// component  should always be called as a tag 
+root.render(<HeaderComponent/>);
