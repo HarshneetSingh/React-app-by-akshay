af89ef8142a8bb2de48501663276fa13dbea98ba
Harshneet <harshneets50@gmail.com>
2023-05-24 13:24:21 -0400

    edit the function of filter bar

--- src/utils/helper.jsx
+++ src/utils/helper.jsx
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { useOutletContext, useSearchParams,useLocation, useHistory } from "react-router-dom";
+import { useOutletContext, useSearchParams, useLocation, useHistory } from "react-router-dom";
 import LocationContext from "./LocationContext";
 import SortFilterContext from "./SortFilterContext";
 
@@ -45,38 +45,81 @@ export function FilterSelectedBtn(props) {
     const [url, setUrl] = useSearchParams();
     const [selectedSort, setSelectedSort] = useContext(SortFilterContext)
     const [filterArr, setFilterArr] = yo[2]
-    const [hello,sethello]=useState(false)
-  
-    useEffect(()=>{
+    const [hello, sethello] = useState(false)
+
+    useEffect(() => {
         console.log("hiii")
-        if(hello===true){
-            updateRestroByfiltering(filterArr, props.setFilteredRestaurants, setUrl,url, location, selectedSort, setSelectedSort)
+        if (hello === true) {
+            updateRestroByfiltering(filterArr, props.setFilteredRestaurants, setUrl, url, location, selectedSort, setSelectedSort)
             sethello(false);
         }
-    },[hello])
-        return <div className="w-4/5 m-auto">
+    }, [hello])
+    return <div className="w-4/5 m-auto">
         {
             props.filters?.map((category) => {
-                return category?.options.map((filterName) => {
-                    return (filterName?.selected === 1) ? <button
-                        onClick={ () => {
-                            setFilterArr(
-                                [
-                                    { CUISINES: filterArr[0].CUISINES.filter((cuisine) => cuisine !== filterName?.option) },
-                                    { SHOW_RESTAURANTS_WITH: filterArr[1].SHOW_RESTAURANTS_WITH }
-                                ]
-                            )
-                            sethello(true);
+                return category?.options.map((filter, index) => {
+                    return (filter?.selected === 1) ?
+                        <button
+                            onClick={() => {
+                                if (category.key === "CUISINES") {
+                                    setFilterArr(
+                                        [
+                                            { CUISINES: filterArr[0].CUISINES.filter((cuisine) => cuisine !== filter?.option) },
+                                            { SHOW_RESTAURANTS_WITH: filterArr[1].SHOW_RESTAURANTS_WITH }
+                                        ]
 
-                        }}
-                        className="mr-2">{filterName?.option}</button> : ""
+                                    )
+
+                                } else {
+                                    setFilterArr(
+                                        [
+                                            { CUISINES: filterArr[0].CUISINES },
+                                            { SHOW_RESTAURANTS_WITH: filterArr[1].SHOW_RESTAURANTS_WITH.filter((cuisine) => cuisine !== filter?.option) }
+                                        ]
+                                    )
+
+                                }
+                                sethello(true);
+
+                            }}
+                            className="mr-2"
+                        >
+                            {filter?.option}
+                        </button> : ""
                 })
             })
         }
     </div>
 
+
+
+    // filterData?.map((filterName, index) => {
+    //     return (
+    //         <div key={index}>
+    //             <div>{filterName.title}</div>
+    //             <div className="w-full  grid grid-cols-2">
+    //                 {filterName?.options.map((filter, index) => {
+    //                     return (
+    //                         <FilterBarBttn
+    //                             key={index}
+    //                             index={index}
+    //                             filterName={filterName}
+    //                             filterArr={filterArr}
+    //                             setFilterArr={setFilterArr}
+    //                             filter={filter}
+    //                         />
+    //                     );
+    //                 })}
+    //             </div>
+    //         </div>
+    //     );
+    // })
+
+
+
+
 }
-export function updateRestroByfiltering(filterArr, setFilteredRestaurants, setUrl,url, location, selectedSort, setSelectedSort) {
+export function updateRestroByfiltering(filterArr, setFilteredRestaurants, setUrl, url, location, selectedSort, setSelectedSort) {
 
     let urlString = filterArr.map((condition, index) => {
         const conditionName = Object.keys(condition);
@@ -97,26 +140,26 @@ export function updateRestroByfiltering(filterArr, setFilteredRestaurants, setUr
             "%5D";
     });
     urlString = urlString.join("").replace(" ", "%20") + "%7D";
-    updatingFilter(urlString, setFilteredRestaurants, setUrl,url, location, selectedSort, setSelectedSort)
+    updatingFilter(urlString, setFilteredRestaurants, setUrl, url, location, selectedSort, setSelectedSort)
 }
 
-function updatingFilter(urlString, setFilteredRestaurants, setUrl,url, location, selectedSort, setSelectedSort) {
-    if(urlString.includes("3A")){
+function updatingFilter(urlString, setFilteredRestaurants, setUrl, url, location, selectedSort, setSelectedSort) {
+    if (urlString.includes("3A")) {
         setUrl({ filter: `${urlString}` })
         setSelectedSort({
             ...selectedSort,
             filter: urlString
         })
-    }else{
-        urlString=undefined
+    } else {
+        urlString = undefined
         setUrl(url.delete('filter'))
         setSelectedSort({
             ...selectedSort,
             filter: undefined
         })
     }
-    
+
     setFilteredRestaurants([])
     restroSorting(selectedSort.sort, setFilteredRestaurants, location, urlString)
-   
+
 }
